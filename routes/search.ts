cfbc5e09d479a1f937fe5c5221749b9fecd745a2
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-09-25 19:13:27 +0300

    Add endpoint for searching users by username

--- routes/search.ts
+++ routes/search.ts
@@ -7,5 +7,15 @@ import User from '../models/User';
 const router = Router();
 
 router.get('/test', (req: Request, res: Response) => res.json({ msg: 'Search works' }));
-
+router.get('/users', passport.authenticate('jwt', { session: false }),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const searchTerm = req.query.username
+      const matchingUsers = await User.find({ "username": { $regex: searchTerm } }, { username: 1, _id: 1 });
+      return res.status(200).json({ users: matchingUsers });
+    } catch (e) {
+      next(e);
+    }
+  }
+);
 export default router;
